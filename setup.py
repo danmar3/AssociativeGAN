205ee543ac5d7a04563153cff8bba8c9377c3f26
Daniel L. Marino <daniellml55@gmail.com>
2019-01-25 18:07:00 -0500

    added project organisation

---
+++ setup.py
@@ -0,0 +1,29 @@
+try:
+    from pip._internal.operations import freeze
+except ImportError:  # pip < 10.0
+    from pip.operations import freeze
+from setuptools import setup, find_packages
+# for development installation: pip install -e .
+# for distribution: python setup.py sdist #bdist_wheel
+#                   pip install dist/project_name.tar.gz
+DEPS = ['tensorflow-gpu', 'tensorflow-probability', 'pandas', 'twodlearn',
+        'matplotlib', 'jupyter', 'scipy']
+
+
+def get_dependencies():
+    if any(['tensorflow' in installed for installed in freeze.freeze()]):
+        return [dep for dep in DEPS if 'tensorflow' not in dep]
+    else:
+        return DEPS
+
+
+setup(name='acgan',
+      version='0.1',
+      packages=find_packages(
+          exclude=["*test*", "tests"]),
+      package_data={'': ['*.so']},
+      install_requires=get_dependencies(),
+      author='Daniel L. Marino',
+      author_email='marinodl@vcu.edu',
+      licence='GPL',
+      )
