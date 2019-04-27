8870cf9832859e892da40867a81a8283cd311f45
Morgan Stuart <morgansstuart243@gmail.com>
2019-04-27 16:13:34 -0400

    Parameter for activation; grid display nrows

--- acgan/torch_gan.py
+++ acgan/torch_gan.py
@@ -63,7 +63,8 @@ class CNNTransposeBlock(torch.nn.Module):
                     output_padding=0,
                     groups=1,
                     bias=True,
-                    dilation=1):
+                    dilation=1,
+                 activation=None):
         super(CNNTransposeBlock, self).__init__()
 
         self.convt = nn.ConvTranspose2d(in_channels=in_channels,
@@ -77,7 +78,9 @@ class CNNTransposeBlock(torch.nn.Module):
                               dilation=dilation)
         self.bn = nn.BatchNorm2d(out_channels)
         #self.act = nn.ReLU(True)
-        self.act = nn.LeakyReLU(negative_slope=0.2)
+
+        self.act = (nn.LeakyReLU(negative_slope=0.2)
+                    if activation is None else activation)
 
     def forward(self, x):
         out = self.convt(x)
@@ -93,7 +96,9 @@ class CNNBlock(torch.nn.Module):
                     padding=0,
                     dilation=1,
                     groups=1,
-                    bias=True, batchnorm=True):
+                    bias=True, batchnorm=True,
+                 activation=None
+                 ):
         super(CNNBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels=in_channels,
                               out_channels=out_channels,
@@ -107,7 +112,8 @@ class CNNBlock(torch.nn.Module):
         if self.batchnorm:
             self.bn = nn.BatchNorm2d(num_features=out_channels)
         #self.act = nn.ReLU(True)
-        self.act = nn.LeakyReLU(negative_slope=0.2)
+        self.act = (nn.LeakyReLU(negative_slope=0.2)
+                    if activation is None else activation)
 
     def forward(self, x):
         out = self.conv(x)
@@ -230,14 +236,14 @@ class GANTrainer():
         return self.epoch_losses
 
     @staticmethod
-    def grid_display(data, figsize=(10,10), pad_value=0, title=''):
+    def grid_display(data, figsize=(10, 10), pad_value=0, title='',
+                     nrow=8):
         fig, ax = plt.subplots(figsize=figsize)
-
-        #fig = plt.figure(figsize=figsize)
         ax.axis("off")
         ax.set_title(title)
         ax.imshow(np.transpose(vutils.make_grid(data,
                                                 padding=2,
+                                                nrow=nrow,
                                                 normalize=True,
                                                 pad_value=pad_value).detach().cpu(), (1, 2, 0)))
 
