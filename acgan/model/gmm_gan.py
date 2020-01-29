e3290cfa361c618d9b6c184f6c7c60a5f8a26a2f
danmar3 <daniellml55@gmail.com>
2020-01-28 20:29:23 -0500

    working on grad loss

--- acgan/model/gmm_gan.py
+++ acgan/model/gmm_gan.py
@@ -8,29 +8,6 @@ from .msg_gan import (MSG_GAN, MSG_DiscriminatorTrainer, MSG_GeneratorTrainer,
                       AffineLayer, Conv2DLayer, LEAKY_RATE)
 from .base import BaseTrainer
 
-# Taken from
-# https://github.com/NVlabs/stylegan/blob/master/training/loss.py
-def D_logistic_simplegp(G, D, opt, training_set, minibatch_size, reals, labels, r1_gamma=10.0, r2_gamma=0.0): # pylint: disable=unused-argument
-    real_scores_out = D.get_output_for(reals, labels, is_training=True)
-    fake_scores_out = D.get_output_for(fake_images_out, labels, is_training=True)
-    loss = tf.nn.softplus(fake_scores_out)  # -log(1 - logistic(fake_scores_out))
-    loss += tf.nn.softplus(-real_scores_out)  # -log(logistic(real_scores_out)) # temporary pylint workaround # pylint: disable=invalid-unary-operand-type
-
-    if r1_gamma != 0.0:
-        with tf.name_scope('R1Penalty'):
-            real_loss = tf.reduce_sum(real_scores_out)
-            real_grads = tf.gradients(real_loss, [reals])[0]
-            r1_penalty = tf.reduce_sum(tf.square(real_grads), axis=[1, 2, 3])
-        loss += r1_penalty * (r1_gamma * 0.5)
-
-    if r2_gamma != 0.0:
-        with tf.name_scope('R2Penalty'):
-            fake_loss = tf.reduce_sum(fake_scores_out)
-            fake_grads = tf.gradients(fake_loss, [fake_images_out])[0]
-            r2_penalty = tf.reduce_sum(tf.square(fake_grads), axis=[1, 2, 3])
-        loss += r2_penalty * (r2_gamma * 0.5)
-    return loss
-
 
 class GmmDiscriminatorTrainer(MSG_DiscriminatorTrainer):
     @tdl.core.Submodel
@@ -38,6 +15,36 @@ class GmmDiscriminatorTrainer(MSG_DiscriminatorTrainer):
         tdl.core.assert_initialized(self, 'embedding', ['batch_size'])
         sample = self.model.embedding(self.batch_size)
         return sample
+
+    # Taken from
+    # https://github.com/NVlabs/stylegan/blob/master/training/loss.py
+    def _D_logistic_simplegp(self, r1_gamma=10.0, r2_gamma=0.0):
+        real_scores = self.discriminator(self.real_pyramid)
+        sim_scores = self.discriminator(self.sim_pyramid)
+
+        loss_real = tf.keras.losses.BinaryCrossentropy(from_logits=True)(
+                    tf.ones_like(real_scores), real_scores)
+        loss_sim = tf.keras.losses.BinaryCrossentropy(from_logits=True)(
+                    tf.zeros_like(sim_scores), sim_scores)
+        loss = (loss_real + loss_sim)/2.0
+
+        if r1_gamma != 0.0:
+            with tf.name_scope('R1Penalty'):
+                real_loss = tf.reduce_sum(real_scores)
+                real_grads = tf.gradients(real_loss, [real_images])[0]
+                r1_penalty = tf.reduce_sum(tf.square(real_grads),
+                                           axis=[1, 2, 3])
+            loss += tf.reduce_mean(r1_penalty) * (r1_gamma * 0.5)
+
+        if r2_gamma != 0.0:
+            with tf.name_scope('R2Penalty'):
+                fake_loss = tf.reduce_sum(sim_scores)
+                fake_grads = tf.gradients(fake_loss, [fake_images])[0]
+                r2_penalty = tf.reduce_sum(tf.square(fake_grads),
+                                           axis=[1, 2, 3])
+            loss += tf.reduce_mean(r2_penalty) * (r2_gamma * 0.5)
+        assert loss.shape == ()
+        return loss
 
     @tdl.core.OutputValue
     def loss(self, _):
