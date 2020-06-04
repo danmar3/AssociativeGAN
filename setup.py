28edd512816f95d1892cee66c7ec2e76c21d75e0
Daniel L. Marino <daniellml55@gmail.com>
2020-06-04 05:13:12 -0400

    added new version of tensorflow probability for tf 1.15

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ from setuptools import setup, find_packages
 # for development installation: pip install -e .
 # for distribution: python setup.py sdist #bdist_wheel
 #                   pip install dist/project_name.tar.gz
-DEPS = ['tensorflow-probability==0.6.0', 'tensorflow-datasets==1.2.0',
-        'pandas', 'matplotlib', 'jupyter', 'scipy',
+DEPS = ['pandas', 'matplotlib', 'jupyter', 'scipy',
         'attrs', 'Pillow', 'GPUtil', 'scikit-learn',
         'opencv-python']
 
@@ -15,6 +14,8 @@ DEPS_DEV = ['Sphinx', 'sphinx_rtd_theme']
 
 
 def get_dependencies():
+    global DEPS
+
     def is_tensorflow(pkg_name):
         tf_names = ['tensorflow-gpu', 'tensorflow', 'tf-nightly']
         return any(tfname == pkg_name.split('==')[0] for tfname in tf_names)
@@ -22,11 +23,20 @@ def get_dependencies():
     tf_installed = any([is_tensorflow(installed)
                         for installed in freeze.freeze()])
     if tf_installed:
-        version = list(filter(is_tensorflow, freeze.freeze()))[0]
-        if version.split('==')[1] not in ('1.13.1', '1.15.2'):
+        name_version = list(filter(is_tensorflow, freeze.freeze()))[0]
+        version = name_version.split('==')[1]
+        if version not in ('1.13.1', '1.15.2'):
             print('\n[twodlearn]: '
                   'ONLY 1.13.1 VERSION OF TENSORFLOW SUPPORTED!!!\n'
                   'Version 1.15.2 supported only if compiled locally.')
+        if version == '1.13.1':
+            DEPS = DEPS + [
+                'tensorflow-probability==0.6.0', 'tensorflow-datasets==1.2.0']
+        elif version == '1.15.2':
+            DEPS = DEPS + [
+                'tensorflow-probability==0.8.0', 'tensorflow-datasets==1.2.0']
+        else:
+            DEPS = DEPS + ['tensorflow-probability', 'tensorflow-datasets']
         return DEPS
     else:
         return DEPS + ['tensorflow==1.13.1']
