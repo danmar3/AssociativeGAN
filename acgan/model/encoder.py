84dfa9a6f3dcdbc446a9c8b4c92f2c17f37be3dd
Daniel L. Marino <daniellml55@gmail.com>
2020-07-09 06:27:32 -0400

    improved ResConv model. Added option for sparse cross entropy.

--- acgan/model/encoder.py
+++ acgan/model/encoder.py
@@ -26,12 +26,15 @@ def _to_list(value, n_elements):
 
 class ResConv(tdl.core.Layer):
     '''CNN for recovering the encodding of a given image'''
+    # CONV_LAYER = Conv2DLayer
+    CONV_LAYER = tf_layers.Conv2D
+
     embedding_size = tdl.core.InputArgument.required(
         'embedding_size', doc='dimensionality of the embedding.')
 
     @tdl.core.SubmodelInit(lazzy=True)
     def layers(self, units, kernels, strides, dropout=None, padding='same',
-               pooling=None):
+               pooling=None, flatten='global_maxpool', res_layers=2):
         tdl.core.assert_initialized(self, 'layers', ['embedding_size'])
 
         n_layers = len(units)
@@ -43,20 +46,30 @@ class ResConv(tdl.core.Layer):
 
         model = tdl.stacked.StackedLayers()
         for i in range(len(units)):
+            model.add(tf_layers.BatchNormalization())
             # residual
             residual = tdl.stacked.StackedLayers()
-            residual.add(Conv2DLayer(
-                filters=units[i], strides=strides[i],
-                kernel_size=kernels[i], padding=padding[i]))
-            residual.add(tf_layers.LeakyReLU(LEAKY_RATE))
-            if dropout is not None:
-                residual.add(tf_layers.Dropout(rate=dropout))
+            for ri in range(res_layers):
+                if ri != 1:
+                    residual.add(tf_layers.BatchNormalization())
+                residual.add(self.CONV_LAYER(
+                    filters=units[i], strides=strides[i],
+                    kernel_size=kernels[i], padding=padding[i]))
+                residual.add(tf_layers.LeakyReLU(LEAKY_RATE))
+                if dropout is not None:
+                    residual.add(tf_layers.Dropout(rate=dropout))
             # resnet
             model.add(tdl.resnet.ResConv2D(residual=residual))
             if i < len(pooling) and pooling[i] is not None:
                 model.add(tf_layers.MaxPooling2D(pool_size=pooling[i]))
 
-        model.add(tf_layers.Flatten())
+        if flatten == 'flatten':
+            model.add(tf_layers.Flatten())
+        elif flatten == 'global_maxpool':
+            model.add(tf_layers.GlobalMaxPooling2D())
+        else:
+            raise ValueError(f'flatten option {flatten} not recognized')
+        model.add(tf_layers.BatchNormalization())
         model.add(AffineLayer(units=self.embedding_size))
         return model
 
@@ -67,7 +80,7 @@ class ResConv(tdl.core.Layer):
             loc=lambda x: x,
             batch_shape=self.embedding_size)
 
-    def call(self, inputs, expected=False):
+    def call(self, inputs, expected=True):
         tdl.core.assert_initialized(self, 'call', ['layers'])
         output = self.layers(inputs)
         if expected:
@@ -94,34 +107,51 @@ class LinearClassifier(tdl.core.Layer):
             call_fn=lambda model, x: model(x, expected=False))
 
     @tdl.core.SubmodelInit(lazzy=True)
-    def linear(self, **kargs):
-        tdl.core.assert_initialized(self, 'linear', ['n_classes'])
+    def classifier(self, **kargs):
+        tdl.core.assert_initialized(self, 'classifier', ['n_classes'])
         return tf_layers.Dense(units=self.n_classes)
 
     def call(self, inputs, softmax=False):
         encoded = self.encoder(inputs)
-        logits = self.linear(encoded)
+        logits = self.classifier(encoded)
         if softmax:
             return tf.nn.softmax(logits)
         else:
             return logits
 
 
+class MlpClassifier(LinearClassifier):
+    @tdl.core.SubmodelInit(lazzy=True)
+    def classifier(self, units):
+        tdl.core.assert_initialized(self, 'classifier', ['n_classes'])
+        model = tdl.stacked.StackedLayers()
+        for i in range(len(units)):
+            # model.add(tf_layers.BatchNormalization())
+            model.add(tf_layers.Dense(units=units[i]))
+            model.add(tf_layers.LeakyReLU(LEAKY_RATE))
+        model.add(tf_layers.Dense(units=self.n_classes))
+        return model
+
+
 class Accuracy(tdl.core.Layer):
     from_logits = tdl.core.InputArgument.required(
         'from_logits', doc='inputs are logits.')
+    sparse = tdl.core.InputArgument.optional(
+        'sparse', doc='labels are sparse.', default=False)
 
     def call(self, labels, inputs):
         if inputs.shape.as_list()[-1] == 1:
             if self.from_logits:
                 inputs = tf.nn.sigmoid(inputs)
-            comp = tf.equal(tf.round(labels, axis=1),
-                            tf.round(inputs, axis=1))
+            if not self.sparse:
+                labels = tf.round(labels, axis=-1)
+            comp = tf.equal(labels, tf.round(inputs, axis=-1))
         else:
             if self.from_logits:
                 inputs = tf.nn.softmax(inputs)
-            comp = tf.equal(tf.argmax(labels, axis=1),
-                            tf.argmax(inputs, axis=1))
+            if not self.sparse:
+                labels = tf.argmax(labels, axis=-1)
+            comp = tf.equal(labels, tf.argmax(inputs, axis=-1))
         return tf.reduce_mean(tf.cast(comp, tf.float32))
 
 
@@ -134,13 +164,16 @@ class Estimator(tdl.core.TdlModel):
             if value == 'crossentropy':
                 return tf.keras.losses.CategoricalCrossentropy(
                     from_logits=from_logits)
+            elif value == 'sparse_crossentropy':
+                return tf.keras.losses.SparseCategoricalCrossentropy(
+                    from_logits=from_logits)
             else:
                 raise ValueError(f'loss string {value} not recognized.')
         else:
             return value
 
     @tdl.core.SubmodelInit
-    def metrics(self, metrics=None, from_logits=False):
+    def metrics(self, metrics=None, from_logits=False, **kargs):
         if metrics is None:
             return dict()
         if isinstance(metrics, (list, tuple)):
@@ -148,7 +181,9 @@ class Estimator(tdl.core.TdlModel):
             metrics = {name[:3]: name for name in metrics}
         assert isinstance(metrics, dict), \
             'metrics should be specified using a dictionary'
-        metrics_names = {'accuracy': lambda: Accuracy(from_logits=from_logits)}
+        metrics_names = {
+            'accuracy': lambda: Accuracy(from_logits=from_logits, **kargs)
+            }
         return {name: metrics_names[value]()
                 if isinstance(value, str) else value
                 for name, value in metrics.items()}
@@ -181,6 +216,7 @@ class Estimator(tdl.core.TdlModel):
             train_y=tf.compat.v1.placeholder(
                 labels_spec.dtype, shape=labels_spec.shape),
             )
+        tdl.core.initialize_variables(self.model)
 
     @tdl.core.Submodel
     def trainable_variables(self, value):
@@ -199,9 +235,9 @@ class Estimator(tdl.core.TdlModel):
             **kargs)
 
     def get_optimizer(self, train_x, train_y, valid_x=None, valid_y=None,
-                      learning_rate=None):
+                      learning_rate=None, **kargs):
         ops = self._evaluate_graph(train_x, train_y, valid_x, valid_y)
-        return self._get_optimizer(ops, learning_rate=learning_rate)
+        return self._get_optimizer(ops, learning_rate=learning_rate, **kargs)
 
     @tdl.core.SubmodelInit(lazzy=True)
     def optimizer(self, learning_rate=None, **kargs):
@@ -212,7 +248,7 @@ class Estimator(tdl.core.TdlModel):
 
     def fit(self, train_x, train_y, n_steps=100):
         if not tdl.core.is_property_initialized(self, 'train_ops'):
-            self.build(train_x, train_y)
+            self.compile(train_x, train_y)
         tdl.core.assert_initialized(
             self, 'fit', ['train_ops', 'trainable_variables'])
 
@@ -246,3 +282,10 @@ class LinearEstimator(Estimator):
     @tdl.core.SubmodelInit
     def model(self, n_classes, encoder, **kargs):
         return LinearClassifier(n_classes=n_classes, encoder=encoder, **kargs)
+
+
+class MlpEstimator(Estimator):
+    @tdl.core.SubmodelInit
+    def model(self, n_classes, units, encoder, **kargs):
+        return MlpClassifier(n_classes=n_classes, encoder=encoder,
+                             classifier={'units': units}, **kargs)
