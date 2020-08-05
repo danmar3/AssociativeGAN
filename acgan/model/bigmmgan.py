4ced79e3ce3174d3a8ac057ed14f0d1f16687707
Daniel L. Marino <daniellml55@gmail.com>
2020-08-05 00:53:22 -0400

    working on bigmmgan

--- acgan/model/bigmmgan.py
+++ acgan/model/bigmmgan.py
@@ -87,7 +87,7 @@ class Discriminator(tdl.core.Layer):
 
         layers = list()
         for i in range(len(self.units)):
-            layers.append(ResStages101(
+            layers.append(ResBottleneck(
                 stages=stages,
                 units=self.units[i],
                 kernel_size=kernels[i],
@@ -99,7 +99,7 @@ class Discriminator(tdl.core.Layer):
                 equalized=self.equalized
                 ))
         layers.append(MinibatchStddev())
-        layers.append(ResStages101(
+        layers.append(ResBottleneck(
             stages=stages,
             units=self.units[-1],
             kernel_size=3,
@@ -159,5 +159,204 @@ class Discriminator(tdl.core.Layer):
             raise ValueError(f'output {output} not a valid option.')
 
 
+class NewAxis(tdl.core.Layer):
+    @tdl.core.InputArgument
+    def axis(self, value):
+        if value is None:
+            raise ValueError('axis must be specified')
+        assert isinstance(value, (list, tuple))
+        assert all(ai in (..., tf.newaxis) for ai in value)
+        return value
+
+    def compute_output_shape(self, input_shape):
+        tdl.core.assert_initialized(self, 'compute_output_shape', ['axis'])
+        output_shape = list()
+        for ai in self.axis:
+            if ai is tf.newshape:
+                output_shape.append(1)
+            elif ai is ...:
+                output_shape = output_shape + input_shape.as_list()
+            else:
+                raise ValueError('')
+        return tf.TensorShape(output_shape)
+
+    def call(self, inputs):
+        return inputs[self.axis]
+
+
+class TransposeLayer(tdl.core.Layer):
+    @tdl.core.InputArgument
+    def axis(self, value):
+        if value is None:
+            raise ValueError('axes must be specified')
+        assert isinstance(value, (list, tuple))
+        assert all(isinstance(ai, int) for ai in value)
+        return value
+
+    def compute_output_shape(self, input_shape):
+        tdl.core.assert_initialized(self, 'compute_output_shape', ['axis'])
+        return tf.TensorShape(input_shape[ai] for ai in self.axis)
+
+    def call(self, inputs):
+        return tf.transpose(inputs, self.axis)
+
+
 class Generator(tdl.core.Layer):
-    pass
+    units = tdl.core.InputArgument.required(
+        'units', doc='number of units in each hidden block of the network')
+
+    batchnorm = tdl.core.InputArgument.optional(
+        'batchnorm', doc='batch normalization to use.',
+        default='pixelwise')
+    use_bias = tdl.core.InputArgument.optional(
+        'use_bias', doc='use bias', default=True)
+    equalized = tdl.core.InputArgument.optional(
+        'equalized', doc="use equalized version of conv layers",
+        default=True)
+    output_activation = tdl.core.InputArgument.optional(
+        'output_activation', doc="activation function at the output.",
+        default=None)
+    output_channels = tdl.core.InputArgument.optional(
+        'output_channels', doc="number of output channels"
+    )
+
+    @tdl.core.SimpleParameter
+    def layer_lib(self, _):
+        tdl.core.assert_initialized(self, 'layer_lib', ['equalized'])
+        if self.equalized:
+            return equalized.get_layer_lib('equalized')
+        else:
+            return equalized.get_layer_lib('keras')
+
+    @tdl.core.InputArgument
+    def embedding_size(self, value: typing.Union[int, None]):
+        if value is None:
+            tdl.core.assert_initialized(
+                self, 'embedding_size', ['input_shape'])
+            value = self.input_shape.as_list()[-1]
+        return value
+
+    def hidden_shapes(self):
+        tdl.core.assert_initialized(
+            self, 'hidden_shapes', ['embedding_size', 'layers'])
+        _input_shape = tf.TensorShape([None, self.embedding_size])
+        hidden_shapes = list()
+        for layer in self.layers[:-1]:
+            _input_shape = layer.compute_output_shape(_input_shape)
+            hidden_shapes.append(_input_shape)
+        return hidden_shapes
+
+    def output_shape(self):
+        return self.projections[-1].compute_output_shape(
+            self.hidden_shapes()[-1])
+
+    def pyramid_shapes(self):
+        tdl.core.assert_initialized(
+            self, 'pyramid_shapes', ['projections'])
+        shapes = list()
+        for proj, hidden_shape in zip(self.projections, self.hidden_shapes()):
+            shapes.append(proj.compute_output_shape(hidden_shape))
+        return shapes
+
+    @tdl.core.Submodel
+    def projections(self, value):
+        if value is not None:
+            return value
+        tdl.core.assert_initialized(
+            self, 'projections',
+            ['output_activation', 'output_channels', 'use_bias', 'hidden',
+             'layer_lib'])
+        projections = [
+            self.layer_lib.Conv1x1Proj(
+                units=self.output_channels,
+                activation=(self.output_activation() if self.output_activation
+                            else None),
+                use_bias=self.use_bias)
+            for i in range(len(self.hidden))]
+        return projections
+
+    @tdl.core.SubmodelInit(lazzy=True)
+    def input_layer(self, target_shape, batchnorm=None, kernel_size=3):
+        tdl.core.assert_initialized(
+            self, 'input_layer', ['layer_lib', 'use_bias'])
+        # batchnorm
+        if self.batchnorm is not None:
+            batchnorm = self.batchnorm
+        # project
+        layers = tdl.stacked.StackedLayers()
+        layers.add(NewAxis(axis=[..., tf.newaxis, tf.newaxis]))
+        layers.add(TransposeLayer(axis=[0, 3, 2, 1]))
+
+        target_shape = target_shape.as_list()
+        layers.add(self.layer_lib.Conv2DTranspose(
+            filters=target_shape[-1],
+            kernel_size=[target_shape[0], target_shape[1]]
+        ))
+        if batchnorm:
+            layers.add(batchnorm())
+        # conv
+        layers.add(self.layer_lib.Conv2D(
+            filters=target_shape[-1],
+            kernel_size=kernel_size,
+            padding='same',
+            use_bias=self.use_bias
+        ))
+        # layers.add(tf_layers.LeakyReLU(self.leaky_rate))
+        return layers
+
+    @tdl.core.SubmodelInit(lazzy=True)
+    def hidden(self, stages=3, batchnorm=None, dropout=None,
+               kernel_size=3, upsample=2,
+               leaky_rate=0.2):
+        tdl.core.assert_initialized(
+            self, 'hidden', ['units', 'use_bias', 'equalized'])
+        n_layers = len(self.units)
+        # kernels arg
+        assert isinstance(kernel_size, (int, list, tuple))
+        if isinstance(kernel_size, int):
+            kernels = [kernel_size for _ in range(n_layers)]
+        assert all(isinstance(ki, int) for ki in kernels)
+        # upsample arg
+        assert (isinstance(upsample, (int, list, tuple)) or (upsample is None))
+        if isinstance(upsample, (int, dict)):
+            upsample = [upsample for _ in range(n_layers)]
+        assert all(isinstance(pi, (int, dict)) or (pi is None)
+                   for pi in upsample)
+        # batchnorm
+        if self.batchnorm is not None:
+            batchnorm = self.batchnorm
+
+        layers = tdl.stacked.StackedLayers()
+        for i in range(len(self.units)):
+            layers.add(ResBottleneck(
+                stages=stages,
+                units=self.units[i],
+                kernel_size=kernels[i],
+                batchnorm=batchnorm,
+                leaky_rate=leaky_rate,
+                upsample=upsample[i],
+                dropout=dropout,
+                use_bias=self.use_bias,
+                equalized=self.equalized
+                ))
+        return layers
+
+    def call(self, inputs, output='image'):
+        '''the call expects a full pyramid.'''
+        out = inputs
+        if self.input_layer:
+            out = self.input_layer(out)
+        hidden = list()
+        for layer_i in self.hidden.layers:
+            out = layer_i(out)
+            hidden.append(out)
+
+        if output == 'hidden':
+            return hidden
+        elif output == 'image':
+            return self.projections[-1](out)
+        elif output == 'pyramid':
+            return [pi(hi) for hi, pi in zip(hidden, self.projections)]
+
+    def pyramid(self, inputs):
+        return self(inputs, output='pyramid')
