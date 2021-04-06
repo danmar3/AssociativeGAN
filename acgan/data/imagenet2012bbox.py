b3aab1bd0fa9064a05f307443fdc8b1a6256dd7e
Daniel L Marino <daniellml55@gmail.com>
2021-04-05 20:15:17 -0400

    added imagenet bbox labels

--- acgan/data/imagenet2012bbox.py
+++ acgan/data/imagenet2012bbox.py
@@ -24,6 +24,8 @@ import io
 import os
 import tarfile
 
+import xml.etree.ElementTree as ET
+
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets.public_api as tfds
 
@@ -93,6 +95,29 @@ CMYK_IMAGES = [
 PNG_IMAGES = ['n02105855_2933.JPEG']
 
 
+def extract_class_bbox(class_folder):
+    def extract_xml(file):
+        path = os.path.join(class_folder, file)
+        tree = ET.parse(path)
+        data = [{'label': next(obj.iter('name')).text,
+                 'xlim': (int(next(obj.iter('xmin')).text),
+                          int(next(obj.iter('xmax')).text)),
+                 'ylim': (int(next(obj.iter('ymin')).text),
+                          int(next(obj.iter('ymax')).text)),
+                 }
+                for obj in tree.getroot().iter('object')]
+        return data
+    return {file: extract_xml(file)
+            for file in os.listdir(class_folder)}
+
+
+def extract_bbox(bbox_dir):
+    bbox_data = {
+        folder: extract_class_bbox(os.path.join(bbox_dir, folder))
+        for folder in os.listdir(bbox_dir)}
+    return bbox_data
+
+
 class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
     """Imagenet 2012, aka ILSVRC 2012."""
     VERSION = tfds.core.Version(
@@ -114,6 +139,15 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
                 'image': tfds.features.Image(encoding_format='jpeg'),
                 'label': tfds.features.ClassLabel(names_file=names_file),
                 'file_name': tfds.features.Text(),  # Eg: 'n15075141_54.JPEG'
+                'objects': tfds.features.Sequence({
+                    "bbox": tfds.features.FeaturesDict({
+                        'xmin': tf.int32,
+                        'xmax': tf.int32,
+                        'ymin': tf.int32,
+                        'ymax': tf.int32,
+                    }),
+                    "label": tfds.features.Text(),
+                    }),
             }),
             supervised_keys=('image', 'label'),
             homepage='http://image-net.org/',
@@ -124,8 +158,9 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
     def _get_validation_labels(val_path):
         """Returns labels for validation.
         Args:
-          val_path: path to TAR file containing validation images. It is used to
-          retrieve the name of pictures and associate them to labels.
+          val_path: path to TAR file containing validation images.
+          It is used to retrieve the name of pictures and associate them
+          to labels.
         Returns:
           dict, mapping from image name (str) to label (str).
         """
@@ -139,14 +174,22 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
         return dict(zip(images, labels))
 
     def _split_generators(self, dl_manager):
-        train_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2012_img_train.tar')
-        val_path = os.path.join(dl_manager.manual_dir, 'ILSVRC2012_img_val.tar')
-        # We don't import the original test split, as it doesn't include labels.
-        # These were never publicly released.
-        if not tf.io.gfile.exists(train_path) or not tf.io.gfile.exists(val_path):
+        train_path = os.path.join(
+            dl_manager.manual_dir, 'ILSVRC2012_img_train.tar')
+        train_bbox = os.path.join(
+            dl_manager.manual_dir, 'ILSVRC2012_bbox_train_v2.tar.gz')
+        val_path = os.path.join(
+            dl_manager.manual_dir, 'ILSVRC2012_img_val.tar')
+        val_bbox = os.path.join(
+            dl_manager.manual_dir, 'ILSVRC2012_bbox_val_v3.tgz')
+        # We don't import the original test split, as it doesn't include
+        # labels. These were never publicly released.
+        if (not tf.io.gfile.exists(train_path)
+                or not tf.io.gfile.exists(val_path)):
             raise AssertionError(
-                'ImageNet requires manual download of the data. Please download '
-                'the train and val set and place them into: {}, {}'.format(
+                'ImageNet requires manual download of the data. '
+                'Please download the train and val set and place them into: '
+                '{}, {}'.format(
                     train_path, val_path))
 
         return [
@@ -154,12 +197,14 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
                 name=tfds.Split.TRAIN,
                 gen_kwargs={
                     'archive': dl_manager.iter_archive(train_path),
+                    'bbox_folder': dl_manager.extract(train_bbox),
                 },
             ),
             tfds.core.SplitGenerator(
                 name=tfds.Split.VALIDATION,
                 gen_kwargs={
                     'archive': dl_manager.iter_archive(val_path),
+                    'bbox_folder': dl_manager.extract(val_bbox),
                     'validation_labels': self._get_validation_labels(val_path),
                 },
             ),
@@ -175,19 +220,67 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
             image = io.BytesIO(tfds.core.utils.png_to_jpeg(image.read()))
         return image
 
-    def _generate_examples(self, archive, validation_labels=None):
+    @staticmethod
+    def _get_objects(image_fname, bbox):
+        if bbox is None:
+            return list()
+
+        xml_name = image_fname.split('.')[0] + '.xml'
+        class_name = image_fname.split('_')[0]
+        if class_name in bbox and xml_name in bbox[class_name]:
+            return [
+                {'bbox': {
+                    'xmin': obj['xlim'][0],
+                    'xmax': obj['xlim'][1],
+                    'ymin': obj['ylim'][0],
+                    'ymax': obj['ylim'][1],
+                    },
+                 'label': obj['label']}
+                for obj in bbox[class_name][xml_name]
+            ]
+        else:
+            return list()
+
+    @staticmethod
+    def _get_objects_valid(image_fname, bbox):
+        if bbox is None:
+            return list()
+
+        xml_name = image_fname.split('.')[0] + '.xml'
+        if xml_name in bbox:
+            return [
+                {'bbox': {
+                    'xmin': obj['xlim'][0],
+                    'xmax': obj['xlim'][1],
+                    'ymin': obj['ylim'][0],
+                    'ymax': obj['ylim'][1],
+                    },
+                 'label': obj['label']}
+                for obj in bbox[xml_name]
+            ]
+        else:
+            return list()
+
+    def _generate_examples(self, archive, bbox_folder=None,
+                           validation_labels=None):
         """Yields examples."""
         if validation_labels:  # Validation split
             for key, example in self._generate_examples_validation(
-                    archive, validation_labels):
+                    archive, bbox_folder, validation_labels):
                 yield key, example
-        # Training split. Main archive contains archives names after a synset noun.
+        # bbox
+        if bbox_folder is not None:
+            bbox = extract_bbox(bbox_folder)
+        else:
+            bbox = None
+        # Training split. Main archive contains archives names after a
+        # synset noun.
         # Each sub-archive contains pictures associated to that synset.
         for fname, fobj in archive:
             label = fname[:-4]  # fname is something like 'n01632458.tar'
-            # TODO(b/117643231): in py3, the following lines trigger tarfile module
-            # to call `fobj.seekable()`, which Gfile doesn't have. We should find an
-            # alternative, as this loads ~150MB in RAM.
+            # TODO(b/117643231): in py3, the following lines trigger tarfile
+            # module to call `fobj.seekable()`, which Gfile doesn't have.
+            # We should find an alternative, as this loads ~150MB in RAM.
             fobj_mem = io.BytesIO(fobj.read())
             for image_fname, image in tfds.download.iter_archive(
                     fobj_mem, tfds.download.ExtractMethod.TAR_STREAM):
@@ -196,14 +289,20 @@ class Imagenet2012bbox(tfds.core.GeneratorBasedBuilder):
                     'file_name': image_fname,
                     'image': image,
                     'label': label,
+                    'objects': self._get_objects(image_fname, bbox)
                 }
                 yield image_fname, record
 
-    def _generate_examples_validation(self, archive, labels):
+    def _generate_examples_validation(self, archive, bbox_folder, labels):
+        if bbox_folder is not None:
+            bbox = extract_class_bbox(os.path.join(bbox_folder, 'val'))
+        else:
+            bbox = None
         for fname, fobj in archive:
             record = {
                 'file_name': fname,
                 'image': fobj,
                 'label': labels[fname],
+                'objects': self._get_objects_valid(fname, bbox)
             }
             yield fname, record
