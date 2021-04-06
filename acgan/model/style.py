57156a0c48c32f5aac2d0f8f74585d55a9fe6c91
Daniel L Marino <daniellml55@gmail.com>
2021-04-05 20:15:41 -0400

    added placeholder for style gan

---
+++ acgan/model/style.py
@@ -0,0 +1,18 @@
+import twodlearn as tdl
+import tensorflow as tf
+import numpy as np
+
+
+class AdaIN(tdl.core.Layer):
+    ''' Adaptive instance normalization:
+        https://arxiv.org/pdf/1812.04948.pdf
+    '''
+
+    def conv():
+        pass
+
+    def upsample():
+        pass
+
+    def call(inputs, style):
+        pass
